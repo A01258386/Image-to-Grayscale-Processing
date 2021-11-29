c091c12af52066da1889e8b0210c6f77fd3e3354
Gokce Gokmen <51439620+A01258386@users.noreply.github.com>
2021-11-28 20:58:44 -0800

    Grayscaled completed

--- IOhandler.js
+++ IOhandler.js
@@ -31,7 +31,6 @@ const unzip = (pathIn, pathOut) => {
       });
   });
 };
-unzip('myfile.zip', './unzipped')
 
 //create a function an array of file paths for all the png files
 const readDir = (dir) => {
@@ -40,22 +39,19 @@ const readDir = (dir) => {
       if (err) {
         reject(err);
       } else {
-        resolve(files);
+       
         let arr = [];
         files.forEach((file) => {
           if (path.extname(file) === ".png") {
             arr.push(path.join(dir, file));  
-            return arr;          
           }          
         });
-        console.log(arr);
-
+        resolve(arr);    
       }
     });
   });
 };
 
-readDir("./unzipped")
 
 
 
@@ -66,7 +62,6 @@ readDir("./unzipped")
  * @param {string} path
  * @return {promise}
  */
-console.log(readDir("./unzipped"));
 
 /**
  * Description: Read in png file by given pathIn,
@@ -77,13 +72,13 @@ console.log(readDir("./unzipped"));
  * @return {promise}
  */
 
-//create a function called grayScale takes pathIn and pathOut as arguments
-//take array of file paths and save the modified pixel array to grayscled folder
 const grayScale = (filePath, pathProcessed) => {
   return new Promise((resolve, reject) => {
+    console.log("Processing file: " + filePath);
     fs.createReadStream(filePath)
       .pipe(new PNG({ filterType: 4 }))
       .on("parsed", function () {
+        console.log("ok");
         for (let y = 0; y < this.height; y++) {
           for (let x = 0; x < this.width; x++) {
             let idx = (this.width * y + x) << 2;
@@ -93,13 +88,12 @@ const grayScale = (filePath, pathProcessed) => {
             this.data[idx + 2] = avg;
           }
         }
-        this.pack().pipe(fs.createWriteStream(pathProcessed));
+        this.pack().pipe(fs.createWriteStream(pathProcessed+'/'+ path.basename(filePath)));
         resolve();
       });
   });
 };
 
-grayScale("./unzipped", "./grayscaled");
 
 module.exports = {
   unzip,
--- main.js
+++ main.js
@@ -12,3 +12,20 @@ const IOhandler = require("./IOhandler"),
   zipFilePath = `${__dirname}/myfile.zip`,
   pathUnzipped = `${__dirname}/unzipped`,
   pathProcessed = `${__dirname}/grayscaled`;
+
+
+  
+IOhandler.unzip(zipFilePath, pathUnzipped)
+.then(()=>{
+  console.log("unzipped");
+  return IOhandler.readDir(pathUnzipped);
+}).then(async (data)=>{
+  console.log("readDir ./unzipped");
+  console.log("RESULT ",data);
+  for (let i = 0; i < data.length; i++) {
+    IOhandler.grayScale(data[i], pathProcessed)
+  }
+}).catch((err)=>{
+  console.log("error",err);
+});
+    
