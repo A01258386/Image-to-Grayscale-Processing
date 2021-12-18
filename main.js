cdc12b720e7bbb31367a1d6e97182dcbff6db982
Gokce Gokmen <51439620+A01258386@users.noreply.github.com>
2021-12-18 02:01:02 -0800

    https://github.com/A01258386/Image-to-Grayscale-Processing

--- main.js
+++ main.js
@@ -11,21 +11,56 @@
 const IOhandler = require("./IOhandler"),
   zipFilePath = `${__dirname}/myfile.zip`,
   pathUnzipped = `${__dirname}/unzipped`,
-  pathProcessed = `${__dirname}/grayscaled`;
+  pathProcessed = `${__dirname}/grayscaled`,
+  path = require("path");
 
 
-  
-IOhandler.unzip(zipFilePath, pathUnzipped)
-.then(()=>{
-  console.log("unzipped");
-  return IOhandler.readDir(pathUnzipped);
-}).then(async (data)=>{
-  console.log("readDir ./unzipped");
-  console.log("RESULT ",data);
-  for (let i = 0; i < data.length; i++) {
-    IOhandler.grayScale(data[i], pathProcessed)
-  }
-}).catch((err)=>{
-  console.log("error",err);
+
+
+const filter = new Promise((resolve, reject) => {
+  IOhandler.unzip(zipFilePath, pathUnzipped)
+    .then(() => {
+      console.log("unzipped");
+      return IOhandler.readDir(pathUnzipped);
+    }).then(async (data) => {
+      console.log("readDir ./unzipped");
+      console.log("RESULT ", data);
+      for (let i = 0; i < data.length; i++) {
+        IOhandler.grayScale(data[i], pathProcessed)
+      }
+      const files = [];
+      data.forEach(file => {
+        files.push(path.basename(file));
+      });
+      resolve(files);
+    }).catch((err) => {
+      console.log("error", err);
+      reject(err);
+    });
 });
-    
+
+const express = require("express"),
+  app = express(),
+  port = process.env.PORT || 3000;
+//use ejs
+app.set("view engine", "ejs");
+app.use("/unzipped", express.static('unzipped'))
+app.use("/grayscaled", express.static('grayscaled'))
+
+filter.then((fileNames) => {
+
+  app.get("/", (req, res) => {
+    res.render("index", {
+      images: fileNames
+    });
+
+  });
+
+});
+
+
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+}
+);
